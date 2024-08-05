35d10755d7372f4f2d1ffa8cd9f34cd83283f2b5
ysawa <yoshihirosawa@gmail.com>
2024-08-05 18:00:55 +0900

    add: logger

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-from dotenv import load_dotenv
+import logging
 import os
+
 import google.generativeai as genai
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -9,3 +11,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 TRANSCRIPT_LANGUAGES = os.getenv("TRANSCRIPT_LANGUAGES", "ja,en").split(",")
 
 genai.configure(api_key=GEMINI_API_KEY)
+
+logging.basicConfig(
+    format="[%(asctime)s][%(levelname)s] %(message)s in %(pathname)s:%(lineno)d",
+    level=logging.DEBUG
+)
